999b8ad64e20b143eba2e0c4ef672c076d7e9268
Euan Jed Tabamo <tabamoeuan@gmail.com>
2023-10-04 21:04:59 +0800

    update comment

--- python/sample_problems/gcf.py
+++ python/sample_problems/gcf.py
@@ -8,7 +8,7 @@ b = int(input("Enter B:"))
 def compute_GCF(num1, num2):
     gcf = 1
 
-    # choose the smaller number
+    # we can only check divisibility up to the smaller number
     smaller = num1 if num1 < num2 else num2
 
     for i in range(1, smaller + 1): # check divisibility from 1 to smaller
