07e5c6b0677b329adbe4f8b09698e1d597a45d28
Euan Jed Tabamo <tabamoeuan@gmail.com>
2023-10-04 21:10:19 +0800

    Clarify GCF

--- python/sample_problems/gcf.py
+++ python/sample_problems/gcf.py
@@ -12,9 +12,9 @@ def compute_GCF(num1, num2):
     smaller = num1 if num1 < num2 else num2
 
     for i in range(1, smaller + 1): # check divisibility from 1 to smaller
-      if (num1 % i == 0 and num2 % i == 0):
+      if (num1 % i == 0 and num2 % i == 0): # if both are divisible by i, set gcf to i
          gcf = i
-    return gcf
+    return gcf # return the largest number that divides both num1 and num2
 
 gcf = compute_GCF(a,b)
 print(f"The GCF is {gcf}.")
