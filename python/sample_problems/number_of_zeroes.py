5b4443c5e949b010f3fa42a52c369c6ae7687787
Euan Jed Tabamo <tabamoeuan@gmail.com>
2023-10-04 20:40:50 +0800

    Complete number of zeroes

---
+++ python/sample_problems/number_of_zeroes.py
@@ -0,0 +1,12 @@
+# Input an integer n, followed by a list of n integers, and count the number of zeroes in the list.
+
+count = 0
+
+number_of_numbers = int(input("Numbers to input: "))
+
+for i in range(number_of_numbers):
+    number = int(input("Enter a number: "))
+    if number == 0:
+        count += 1
+
+print(f"Zero count: {count}")
