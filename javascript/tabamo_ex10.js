22796db0d63bd6cf084cbe24a2c306ba5a49ee93
Euan Jed Tabamo <79002463+u1tbmo@users.noreply.github.com>
2023-11-21 04:50:22 +0000

    Update Docs

--- javascript/tabamo_ex10.js
+++ javascript/tabamo_ex10.js
@@ -1,5 +1,13 @@
 /* Tabamo, Euan Jed S. | B-1L
 Exercise 10 | November 21, 2023 */
+/**
+ * {DataTypeHere} - Note: used for usual data type declaration.
+ * {(OneDataType|AnotherDataType)} - Note: used for cases where data type could be either of the two.
+ * {DataType[]} - Note: used for an array of DataType instances.
+ * {?DataTypeHere} - Note: used for data types which could be the data type mentioned or null.
+ * {DataTypeHere} [parameterNameHere] - Note: used for optional parameters.
+ * {Object.<KeyDataType, ValueDataType>} - Note: used for an object with KeyDataType keys and ValueDataType values
+ */
 
 const HOME = [3, 4];
 let locationObject = {
@@ -9,7 +17,12 @@ let locationObject = {
   Dustin: [15, 12],
 };
 let distanceArray = [];
-
+/**
+ * Finds the distance using the distance formula.
+ * @param {Number[]} origin - The coordinates of the origin point.
+ * @param {Number[]} point - The coordinates of a point.
+ * @return {Number} The distance between the origin and the point.
+ */
 function findDistance(origin, point) {
   let x2 = origin[0];
   let y2 = origin[1];
@@ -17,6 +30,13 @@ function findDistance(origin, point) {
   let y1 = point[1];
   return Number(Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2).toFixed(3));
 }
+
+/**
+ * Collects and manages the specified origin array and all othercoordinates.
+ * @param {Number[]} origin - The coordinates of the origin point.
+ * @param {Object.<String,Number[]>} locations - The object containing persons and their coordinates
+ * @return {Number[]} The array of distances.
+ */
 function processMap(origin, locations) {
   let entryArray = Object.entries(locations);
   let personArray = [];
@@ -31,6 +51,12 @@ function processMap(origin, locations) {
   // console.log(distanceArray);
   return distanceArray;
 }
+/**
+ * Finds the farthest distance.
+ * @param {Number[]} origin - The coordinates of the origin point.
+ * @param {Object.<String,Number[]>} locations - The object containing persons and their coordinates
+ * @return {Array} The array containing the farthest person's name and their distance.
+ */
 function findFarthest(origin, locations) {
   let entryArray = Object.entries(locations);
   let personArray = [];
@@ -56,6 +82,13 @@ function findFarthest(origin, locations) {
   );
   return [farthestPerson, farthestDistance];
 }
+
+/**
+ * Finds the nearest distance.
+ * @param {Number[]} origin - The coordinates of the origin point.
+ * @param {Object.<String,Number[]>} locations - The object containing persons and their coordinates
+ * @return {Array} The array containing the nearest person's name and their distance.
+ */
 function findNearest(origin, locations) {
   entryArray = Object.entries(locations);
   let personArray = [];
@@ -84,7 +117,9 @@ function findNearest(origin, locations) {
 
   return [nearestPerson, nearestDistance];
 }
-
+/**
+ * The main function.
+ */
 function main() {
   findFarthest(HOME, locationObject);
   findNearest(HOME, locationObject);
@@ -94,4 +129,5 @@ function main() {
   distanceArray = processMap(HOME, locationObject);
 }
 
+// Calls the main function.
 main();
