4219a002122f58bec0bfe1e17febcf96d12b4135
Euan Jed Tabamo <tabamoeuan@gmail.com>
2023-12-16 09:29:04 +0800

    Remove unused files and data

--- python/week7/random.py
+++
@@ -1,7 +0,0 @@
-key = 3
-# create a string of the alphabet
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-# create a rotated alphabet using the key by using string slicing
-cipher = alphabet[key:] + alphabet[:key]
-print(f"{alphabet}")
-print(f"{cipher}")
